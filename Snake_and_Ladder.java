85ff70dc449edda2d416dca931abd45c844f7b1a
annie <ruthannie3@gmail.com>
2020-09-22 22:20:45 +0530

    [annie]Added: Roll the die to get a random number

--- Snake_and_Ladder.java
+++ Snake_and_Ladder.java
@@ -1,6 +1,10 @@
 public class Snake_and_Ladder{
 	public static void main(String[] args){
+		// variables
 		int position = 0;
 		System.out.println("Player is at position: " + position);
+		// Computation
+		int number = (int) (Math.floor(Math.random() * 10) % 6) + 1;
+		System.out.println("Any no.between 1 to 6 = "+ number);
 	}
 }
