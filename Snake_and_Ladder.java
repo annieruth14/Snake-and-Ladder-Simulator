d15a5ba9b1acba176411cab446234f20899b76c7
annie <ruthannie3@gmail.com>
2020-09-22 22:28:14 +0530

    [annie]Added: cases of No play, Ladder and Snake

--- Snake_and_Ladder.java
+++ Snake_and_Ladder.java
@@ -1,10 +1,30 @@
 public class Snake_and_Ladder{
+	public static final int NO_PLAY = 0;
+	public static final int LADDER = 1;
+	public static final int SNAKE = 2;
+
 	public static void main(String[] args){
-		// variables
 		int position = 0;
 		System.out.println("Player is at position: " + position);
-		// Computation
+
 		int number = (int) (Math.floor(Math.random() * 10) % 6) + 1;
 		System.out.println("Any no.between 1 to 6 = "+ number);
+
+		int option = (int) (Math.floor(Math.random() * 10 )) % 3;
+		switch (option) {
+			case NO_PLAY:
+				position = position;
+				System.out.println("Case: No play");
+				break;
+			case LADDER:
+				position = position + number;
+				System.out.println("Case: Ladder");
+				break;
+			case SNAKE:
+				position = position - number;
+				System.out.println("Case: Snake");
+				break;
+		}
+		System.out.println("Final position: " + position);
 	}
 }
