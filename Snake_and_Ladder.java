968812da13aa0508635c365f5e59a458c1e8299e
annie <ruthannie3@gmail.com>
2020-09-22 22:38:00 +0530

    [annie]Modified: Repeat till player reaches position 100

--- Snake_and_Ladder.java
+++ Snake_and_Ladder.java
@@ -1,4 +1,5 @@
 public class Snake_and_Ladder{
+	// Constants
 	public static final int NO_PLAY = 0;
 	public static final int LADDER = 1;
 	public static final int SNAKE = 2;
@@ -6,25 +7,45 @@ public class Snake_and_Ladder{
 	public static void main(String[] args){
 		int position = 0;
 		System.out.println("Player is at position: " + position);
+		// Computation
+		while (position!=100){
+			int number = (int) (Math.floor(Math.random() * 10) % 6) + 1;
+			System.out.println("Any no.between 1 to 6 = "+ number);
 
-		int number = (int) (Math.floor(Math.random() * 10) % 6) + 1;
-		System.out.println("Any no.between 1 to 6 = "+ number);
-
-		int option = (int) (Math.floor(Math.random() * 10 )) % 3;
-		switch (option) {
-			case NO_PLAY:
-				position = position;
-				System.out.println("Case: No play");
-				break;
-			case LADDER:
-				position = position + number;
-				System.out.println("Case: Ladder");
-				break;
-			case SNAKE:
-				position = position - number;
-				System.out.println("Case: Snake");
-				break;
+			int option = (int) (Math.floor(Math.random() * 10 )) % 3;
+			switch (option) {
+				case NO_PLAY:
+					position = position;
+					System.out.println("Case: No play");
+					break;
+				case LADDER:
+					position = position + number;
+					System.out.println("Case: Ladder");
+					break;
+				case SNAKE:
+					position = position - number;
+					System.out.println("Case: Snake");
+					if(position < 0){
+						position = 0;
+					}
+					break;
+			}
+			System.out.println("Final position = " + position);
 		}
-		System.out.println("Final position: " + position);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
